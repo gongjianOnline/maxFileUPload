76a1b4438f0bb7ee960add8580fafb6a230805c1
“gongjian” <15031617367@163.com>
2024-02-22 20:43:04 +0800

    node端可接受切片文件

--- upload_server/routes/index.js
+++ upload_server/routes/index.js
@@ -1,13 +1,27 @@
 const router = require('koa-router')()
+const fs = require("fs")
 
 router.post('/upload/:fileName', async (ctx, next) => {
   const chunkFileName = ctx.params.fileName;
   const fileName = ctx.request.query;
+  let params = [];
+  ctx.req.on('data', async (chunk) => {
+      const buffer = Buffer.from(chunk)
+      console.log(buffer)
+      // await fs.mkdir(`./uploads`, { recursive: true });
+      await fs.writeFileSync(`./uploads/${fileName.chunkFileName}`,buffer);
+  })
+  // ctx.req.on('end', (chunk) => {
+  //   // console.log(params)
+  //     // let buffer = Buffer.concat(params);
+  //     // fs.writeFileSync(`./public/uploads/111.png`,buffer);
+  // })
   ctx.body = {
     code:101,
     data:{
       chunkFileName,
-      fileName
+      fileName,
+      body:ctx.request.body
     },
     message:"请求成功"
   }
